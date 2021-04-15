f686c7f92470852116250d53bf2a72287e8f7a66
Ovab <bavo.famknol@gmail.com>
2021-04-16 00:10:04 +0200

    Made some logo's and screwed with css

--- index.php
+++ index.php
@@ -3,17 +3,17 @@
 include 'header.php';
 echo '<div class="Catagories">
 <a href="category.php/?id=1">
-<img alt="games catagory" src="images/games_logo%20(2).png" width="200px">
+<img alt="games catagory" src="images/gaming2.0.png">
 </a>
 
 <a class="Catagories" href="category.php/?id=2">
-<img alt="Random catagory" src="images/ranom_logo%20(2).png" width="200px">
-</a>
-<a href="category.php/?id=4">
-<img alt="W3Schools" src="images/tech2.0.png"200px" height="200px">
+<img alt="Random catagory" src="images/random2.0.png">
 </a>
 <a href="category.php/?id=3">
-<img alt="W3Schools" src="images/bloc.png" width="200px">
+<img alt="W3Schools" src="images/tech2.0.png">
+</a>
+<a href="category.php/?id=4">
+<img alt="W3Schools" src="images/programming2.0.png">
 </a>
 </div>
 ';
